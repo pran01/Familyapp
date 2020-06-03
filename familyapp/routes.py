9948a73da87f13abc4db2630c5614370f8744b9e
Pranav Sinha <pran.sinha1.0@gmail.com>
2020-06-03 19:44:55 +0530

    Adding Family tree

---
+++ familyapp/routes.py
@@ -0,0 +1,10 @@
+from Familyapp.familyapp import app
+from flask import render_template,url_for
+
+@app.route("/",methods=['GET','POST'])
+def getaccess():
+    return render_template('getaccess.html')
+
+@app.route("/",methods=['GET','POSt'])
+def familytree():
+    return render_template('familytree.html')
